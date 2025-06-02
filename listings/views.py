0097a9b594ed03f74a22607905bd12a6a0e44f72
shing <mylession002@gmail.com>
2025-06-02 21:19:01 +0800

    step 9a : search html choices

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from listings.models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+
 # Create your views here.
 def index(request):
     listings = Listing.objects.all()
